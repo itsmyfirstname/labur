fee74f9297861aca7143987336110485abbb4aa3
itsmyfirstname <37390932+itsmyfirstname@users.noreply.github.com>
2024-08-30 15:22:24 -0400

    initial

---
+++ module/rust/hello-world/main.rs
@@ -0,0 +1,8 @@
+/*
+The main function is special: it is always the first code that runs in every executable Rust program. 
+Here, the first line declares a function named main that has no parameters and returns nothing. 
+If there were parameters, they would go inside the parentheses ().
+*/
+fn main() {
+    println!("Hello, world!");
+}
